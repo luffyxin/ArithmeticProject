855ffd4f1413e23acbf317ab31c4e43d65a84c10
luffyxin <937443942@qq.com>
2018-03-24 19:44:04 +0800

    尼姆更新

--- src/IndefiniteEquation.java
+++ src/IndefiniteEquation.java
@@ -1,7 +1,33 @@
-/**
- * Created by Administrator on 2018/3/24.
- * 解不定方程
- */
-public class IndefiniteEquation {
 
+// 尼姆堆的模2加(异或)解法
+/*
+    10
+   101
+  1100
+  1110
+--------
+  0101
+*/
+public class IndefiniteEquation
+{
+    static void f(int[] a){
+        int sum = 0;
+        for(int i=0; i<a.length; i++){
+            sum ^= a[i];
+        }
+        if(sum==0){
+            System.out.println("输了");
+            return;
+        }
+
+        for(int i=0; i<a.length; i++){
+            int x = sum ^ a[i];
+            if(x<a[i]) System.out.println(a[i] + " --> " + x);
+        }
+    }
+
+    public static void main(String[] args){
+        int[] a = {2,5,12,14};
+        f(a);
+    }
 }
